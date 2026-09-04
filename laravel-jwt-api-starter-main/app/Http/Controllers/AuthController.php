5c1cf2774293dc9567ef7a7d86ece6d73c04753d
andres <andresnarvaezortiz7@gmail.com>
2026-09-04 13:03:49 +0200

    Delete kafka server

--- laravel-jwt-api-starter-main/app/Http/Controllers/AuthController.php
+++ laravel-jwt-api-starter-main/app/Http/Controllers/AuthController.php
@@ -11,7 +11,6 @@ use Illuminate\Support\Facades\Hash;
 use App\Support\Traits\Authenticatable;
 use App\Support\Exceptions\OAuthException;
 use Tymon\JWTAuth\Facades\JWTAuth;
-use App\Services\KafkaProducerService;
 
 class AuthController extends Controller
 {
@@ -57,8 +56,6 @@ class AuthController extends Controller
                 code: 'invalid_credentials_provided'
             );
         }
-
-        app(KafkaProducerService)->send('test-topic', 'login desde auth-service');
 
         return $this->responseWithToken($token);
     }
